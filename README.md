ffc604a33a772224827af88532953867ed4df8c8
flex-pgum <56337995+flex-pgum@users.noreply.github.com>
2022-07-13 17:21:40 +0200

    fork from original repo (v2.0.2) updated css_styles.min.css

---
+++ README.md
@@ -0,0 +1,19 @@
+# GLPI Customization Plugin
+
+## INFO
+**GLPI version support starting from 9.5.8**
+
+For older version support (< 9.5.8) head over to the original repository.
+For newer version support (> 9.5.x) switch to main branch or download the release from the release page
+
+This repository is a fork
+of [https://github.com/stdonato/glpi-modifications](https://github.com/stdonato/glpi-modifications). As the original
+owner of this repository does not continue to support future versions of GLPI, we decided to maintain it.
+We have rewritten most of the code but the functionalities stay more or less the same.
+
+This plugin and it's maintainers are not connected to the GLPI project in any way.
+
+## FEATURE LIST
+
+- replace GLPI logo of login page and menu with custom logo
+- add background to login page
---
+++ hook.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+This file is part of the GLPI Modifications plugin.
+The glpi-modifications plugin is free software;
+you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+PGUM srl; either version 2 of the License, or
+(at your option) any later version of the GNU General Public License.
+The glpi-modifications plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with glpi-modifications. If not, see <http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------
+@package	glpi-modifications
+@author		PGUM srl (https://github.com/i-Vertix/glpi-modifications)
+@author		Stevenes Donato (https://github.com/stdonato/glpi-modifications)
+@copyright	Copyright (c) 2022 PGUM srl
+@license	GPLv3
+            http://www.gnu.org/licenses/gpl.txt
+@link		https://github.com/i-Vertix/glpi-modifications
+@link		https://github.com/stdonato/glpi-modifications
+@link		https://i-vertix.com
+@link		http://www.glpi-project.org/
+@since		2022
+--------------------------------------------------------------------------
+*/
+
+/**
+ * @name plugin_mod_install
+ * @access public
+ * @return boolean
+ */
+
+function plugin_mod_install(){
+		
+	//include('install.php');        	
+	return true;
+}
+
+function plugin_mod_uninstall(){
+		
+	//include('uninstall.php');
+	return true;
+}
+
+?>
