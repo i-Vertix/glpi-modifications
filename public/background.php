4bf5324add98cb9e46a0882d40e399b42df3fd5f
flex <felix.geier@pgum.eu>
2026-03-16 11:44:09 +0100

    - update copyright

--- public/background.php
+++ public/background.php
@@ -22,7 +22,7 @@
  * You should have received a copy of the GNU General Public License
  * along with "UI Branding plugin for GLPI". If not, see <http://www.gnu.org/licenses/>.
  * -------------------------------------------------------------------------
- * @copyright Copyright (C) 2025 by i-Vertix/PGUM.
+ * @copyright Copyright (C) 2026 by i-Vertix/PGUM.
  * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
  * @link      https://github.com/i-Vertix/glpi-modifications
  * -------------------------------------------------------------------------
